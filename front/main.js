bebb2cd41ac674f3ebe0969670c7c690fb2fbade
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2026-06-20 00:11:51 +0200

    visibility test

--- front/main.js
+++ front/main.js
@@ -9,7 +9,6 @@ import {askNewQues} from "./js/game.js";
 const welcome = document.getElementById("welcome-page");
 const game = document.getElementById("game-page");
 
-// document.documentElement.style.visibility = 'visible';
 document.addEventListener("DOMContentLoaded", () => {
     if (welcome) {
         choosePlayer();
