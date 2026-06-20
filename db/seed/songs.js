baf9d18e559a6c3c35b2a0855c91b9bf2e152126
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2026-06-20 13:59:31 +0200

    music setup

---
+++ db/seed/songs.js
@@ -0,0 +1,34 @@
+const players = [
+    {
+        name: "Noé",
+        avatar: "noe.svg",
+        score: 0
+    },
+    {
+        name: "Léon",
+        avatar: "leon.svg",
+        score: 0
+    },
+    {
+        name: "Joseph",
+        avatar: "joseph.svg",
+        score: 0
+    },
+    {
+        name: "Benben",
+        avatar: "benben.svg",
+        score: 0
+    },
+    {
+        name: "Mamie",
+        avatar: "grand-ma.svg",
+        score: 0
+    },
+    {
+        name: "Betty",
+        avatar: "betty.svg",
+        score: 0
+    },
+];
+
+export default players;
